3c1f5901d7fcf8f9f3cd0715146b39925633c81f
ssssstella <wt.su77@gmail.com>
2023-08-10 23:56:11 -0700

    add mongo connection

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const{ MongoClient } = require('mongodb');
+
+const MONGODB_URL = "mongodb+srv://admin:sneat123@cluster0.ea14pao.mongodb.net/?retryWrites=true&w=majority";
+
+let client = new MongoClient(MONGODB_URL);
+
 // Add Express
 const express = require("express");
 // Initialize Express
@@ -11,6 +17,30 @@ app.get("/", (req, res) => {
 app.get("/center", (req, res) => {
     res.send("this is my center route");
   });
+
+  async function run() {
+    try {
+        const db = client.db("sample_mflix");
+ 
+        const movies = await db
+            .collection("movies")
+            .find({})
+            .sort({ metacritic: -1 })
+            .limit(10)
+            .toArray();
+        
+        app.get('/movies', async (req, res) => {
+            res.json(movies);
+        });
+ 
+        
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+      }
+    }
+    run().catch(console.dir);
+
 
 // Initialize server
 app.listen(8080, () => {
