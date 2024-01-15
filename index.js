215c2b9b2aa2715cd50cc20003d347e142baba53
ssssstella <wt.su77@gmail.com>
2024-01-14 22:11:36 -0800

    update files

--- index.js
+++ index.js
@@ -1,53 +1,150 @@
-const{ MongoClient } = require('mongodb');
-
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
-  const MONGODB_URL = process.env.MONGODB_URL;
 }
 
-let client = new MongoClient(MONGODB_URL);
-
 // Add Express
-const express = require("express");
-// Initialize Express
+const express = require('express');
 const app = express();
-// Create GET request
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+const md5 = require('md5');
+const jwt = require('jsonwebtoken');
+const secret = 'fbsubjiwqij';
+const cors = require('cors');
+
+// DB
+const mongoose = require('mongoose');
+const MONGODB_URL = process.env.MONGODB_URL;
+mongoose.connect(MONGODB_URL); 
+mongoose.connection.on('open', () => {
+  console.log('database connection succeeded');
+})
+mongoose.connection.on('error', () => {
+  console.log('database connection failed');
+})
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+});
+const userModel = mongoose.model("user", userSchema, "users");
+
+const dashboardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: Map,
+    required: true
+  }
+});
+const dashboardModel = mongoose.model("dashboard", dashboardSchema, "dashboard");
+
+
+// middleman
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.get('/', (req, res) => {
+  res.send('Express on Vercel');
+  // userModel.find().then(data => {
+  //   console.log(data);
+  // })
 });
 
 
-app.get("/center", (req, res) => {
-    res.send("this is my center route");
-  });
+app.get('/center', (req, res) => {
+  res.send('this is my center route');
+});
 
-  async function run() {
-    try {
-        const db = client.db("sample_mflix");
- 
-        const movies = await db
-            .collection("movies")
-            .find({})
-            .sort({ metacritic: -1 })
-            .limit(10)
-            .toArray();
-        
-        app.get('/movies', async (req, res) => {
-            res.json(movies);
-        });
- 
-        
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-      }
+
+app.get('/api/dashboard/:name', (req, res) => {
+  // console.log(req.params);
+  dashboardModel.findOne({name: req.params.name}).then(data => {
+    console.log(data);
+    res.json({
+      code: 1,
+      message: 'fetched data successfully',
+      data: data
+    });
+  }).catch(err => {
+    console.log(err);
+    res.json({
+      code: 0,
+      message: 'fetching data failed',
+      data: err
+    });
+  })
+});
+
+const passCrypt = (req, res, next) => {
+  const password = req.body.password;
+  req.body.password = md5(md5(password).slice(4, 14) + password);
+  next()
+}
+
+app.post('/api/register', passCrypt, (req, res) => {
+  console.log(req.body);
+  userModel.create(req.body).then(data => {
+    console.log('a user was registered successfully ');
+    res.json({
+      code: 1,
+      message: 'registered successfully',
+      data: data
+    });
+  }).catch(err => {
+    console.log(err);
+    res.json({
+      code: 0,
+      message: 'registration failed',
+      data: err
+    });
+  })
+});
+
+app.post('/api/login', passCrypt, (req, res) => {
+  console.log(req.body);
+  userModel.findOne(req.body).then(data => {
+    if (data) {
+      res.send({
+        code: 1,
+        message: 'logged in successfully',
+        token: jwt.sign({ uid: data._id, exp: Math.ceil(Date.now() / 1000) + 7200 }, secret)
+      })
+    } else {
+      res.send({
+        code: 0,
+        message: 'login failed'
+      })
     }
-    run().catch(console.dir);
+  }).catch(err => {
+    console.log(err);
+    res.json({
+      code: 0,
+      message: 'login failed',
+      data: err
+    });
+  })
+});
+
 
 
 // Initialize server
 app.listen(8080, () => {
-  console.log("Server running on port 8080.");
+  console.log('Server running on port 8080.');
 });
 
 module.exports = app;
